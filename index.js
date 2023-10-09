c873cc61d2e8f147c2fb7f177af568b394a9f865
anisa1402 <annisakhoirun2@gmail.com>
2023-10-09 11:26:24 +0700

    endpoint

--- index.js
+++ index.js
@@ -2,10 +2,24 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+
+
+const jalankanServer = async() => {
+  try {
+    app.get('/user', (req, res) => {
+  // res.send('Hello World!')
+
+  let user = [];
+  res.json(user);
 })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+  } catch (error) {
+    
+  }
+};
+
+jalankanServer();
+
